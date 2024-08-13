06cdaa3f87d3ae10c549c31aade1456d170e5256
unknown <darkonex>
2024-08-13 14:31:35 +0100

    change name of an html

--- Pages/views.py
+++ Pages/views.py
@@ -6,19 +6,19 @@ def index0(request, *args, **kwargs):
     return render(request, "index.html", {})
 
 def index1(request, *args, **kwargs):
-    return render(request, "runok\index.html", {})
+    return render(request, "runok/index.html", {})
 
 def index2(request, *args, **kwargs):
-    return render(request, "runok\index-2.html", {})
+    return render(request, "runok/index-2.html", {})
 
 def index3(request, *args, **kwargs):
-    return render(request, "runok\index-3.html", {})
+    return render(request, "runok/index-3.html", {})
 
 def index4(request, *args, **kwargs):
-    return render(request, "runok\index-4.html", {})
+    return render(request, "runok/index-4.html", {})
 
 def index5(request, *args, **kwargs):
-    return render(request, "runok\index-5.html", {})
+    return render(request, "runok/index-5.html", {})
 
 def index6(request, *args, **kwargs):
-    return render(request, "runok\index-6.html", {})
+    return render(request, "runok/index-6.html", {})
